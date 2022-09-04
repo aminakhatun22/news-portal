0a7bf33834dafca584521394fd52f70b7e0ea880
aminakhatun22 <aminak26802@gmail.com>
2022-09-04 10:41:30 +0600

    added newses

--- js/index.js
+++ js/index.js
@@ -29,12 +29,14 @@ const displayCategories = categories => {
 
 const loadCategoriesId = async (category_id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
+
     const res = await fetch(url);
     const data = await res.json();
 
     shownewsDetails(data.data);
 
 }
+
 loadCategoriesId();
 
 const shownewsDetails = async (details) => {
@@ -42,35 +44,41 @@ const shownewsDetails = async (details) => {
 
 
     const foundItems = document.getElementById('found-category');
+    foundItems.innerText = details.length + " " + "Items found";
+
     detailsContainer.textContent = '';
+
     details.forEach(detail => {
-        foundItems.innerText = details.length + " " + "Items found";
 
         const detailDiv = document.createElement('div');
         detailDiv.classList.add('news-div');
+
         detailDiv.innerHTML = `
-       
-        <div class="card card-side bg-base-100 shadow-xl mb-4 ml-28 mr-28 mt-6">
-  <figure><img src="${detail.rating.thumbnail_url}" alt=" "></figure>
-  <div class="card-body ml-6">
-    <h2 class="card-title">${detail.rating.title}</h2>
-    <p>${detail.author.details}</p>
-    <div> 
+      
+      
+       <div class="card card-side bg-base-100 shadow-xl mb-4 ml-28 mr-28 mt-6 ">
+       <div class="flex">
+       <img  class="w-50" src="${detail?.thumbnail_url}" alt=" ">
+       <div class="card-body ml-6">
+        <h2 class="card-title">${detail?.title}</h2>
+        <p>${detail?.details}</p>
+        <div> 
+        
+        <div class= "flex">
+        <img class="w-20 mr-4  rounded-circle" src="${detail.author.img}" alt="">
+    <p class="mt-4">${detail.author.name} </p>
+    <p class="mt-4"> ${detail.total_view}</p>
     <div class="card-actions justify-end">
-    <button class="btn btn-primary data-bs-toggle="modal" data-bs-target="#exampleModal">Show detail</button>
-
-    <div>
-    <img src="" alt="">
-<p></p>
-<p></p>
-</div>
-
-   
-
+        <button class="btn btn-primary data-bs-toggle="modal" data-bs-target="#exampleModal">Show detail</button>
     </div>
-  </div>
-</div>
-
+    </div>
+    
+       
+    
+        </div>
+      </div>
+    </div>
+</div>    
         
         `;
         toggleSpinner(true);
@@ -82,6 +90,7 @@ const shownewsDetails = async (details) => {
 
 
 }
+
 
 
 
@@ -120,7 +129,7 @@ const loadCategories2 = async () => {
     const url = `https://openapi.programming-hero.com/api/news/category/01`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    console.log(data.data);
 }
 loadCategories2();
 
